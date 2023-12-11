fa12c339e9da36005bb151450f5f98d64f8594b4
dahmadi <70730092+dahmadi@users.noreply.github.com>
2023-12-11 00:19:34 -0600

    nav bar elements added, added some styling

--- my-portfolio/src/components/Layout/index.js
+++ my-portfolio/src/components/Layout/index.js
@@ -1,7 +1,13 @@
-import './index.scss';
+import './index.scss'
+import React from 'react'
+import Sidebar from '../Sidebar'
 
 const Layout = () => {
-    return <>Hello</>
+    return (
+        <>
+        <Sidebar /> 
+        </>
+    )
 }
 
 export default Layout
